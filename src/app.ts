4f8fa527d128f6af9828efb5f84983a711da30a1
WONTAE\spald <wontae.gramin9@gmail.com>
2025-02-09 15:14:16 +0000

    [Middleware] Application level: Functions that work between a request and a response Check the passwords, change the formats of the data, shows erros message...

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import { calculatorRouter, healthRouter } from "./routes";
+import { logger } from "./middlewares";
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// [Middleware] express는 top-down이기때문에 middleware를 전부 router 상단에,
+app.use(logger);
 
 app.use("/health", healthRouter);
 app.use("/calculator", calculatorRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello world.");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
