c1f3da1faba08ccb6ff5db86a96e108a7c096c8d
WONTAE\spald <wontae.gramin9@gmail.com>
2025-02-09 13:43:00 +0000

    [Req] request path를 args로 받는 법

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,10 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello world.");
+});
+app.get("/:id", (req, res) => {
+  res.send(`Hello ${req.params.id}`);
+});
