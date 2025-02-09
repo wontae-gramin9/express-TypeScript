8fbaaf1e609c3c5ba0ef0f0b55ceffde66336aa1
WONTAE\spald <wontae.gramin9@gmail.com>
2025-02-09 16:29:59 +0000

    [Middleware] Middleware에서 정의한 req의 timestamp를 router에서도 받아서 사용하기

--- src/routes/calculator.ts
+++ src/routes/calculator.ts
@@ -1,10 +1,11 @@
-import { Router } from "express";
+import { Request, Router } from "express";
 
 export const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res) => {
   res.send({
     message: "Get all calculations",
+    timestamp: req.timestamp,
     data: [
       {
         id: 1,
@@ -18,8 +19,9 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req: Request, res) => {
   res.send({
+    timestamp: req.timestamp,
     message: "Get calculation by ID",
     id: req.params.id,
     result: 1,
