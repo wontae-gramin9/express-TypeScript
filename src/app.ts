d8afa1e963049dae1da455f43cd0f4e9f0135335
WONTAE\spald <wontae.gramin9@gmail.com>
2025-02-09 14:12:37 +0000

    [Endpoints] top-down overwrite를 피하려면, resource를 구분해주는것이 가장 편하다

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello world.");
 });
 
-app.get("/:id", (req, res) => {
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.get("/calculator/:id", (req, res) => {
   // Requset information
   // Query is a key-value object, hence serialize
   console.log(`Query parameter: ${JSON.stringify(req.query)}`);
@@ -21,9 +25,4 @@ app.get("/:id", (req, res) => {
     .status(202)
     .header({ "X-Custom-Header": "foo", "X-Custom-Header-2": "bar" })
     .send(`Hello ${req.params.id}`);
-});
-
-// Top-Down으로 들어가기때문에, /:id에 의해 가려지게 된다
-app.get("/health", (req, res) => {
-  res.send("OK");
 });
